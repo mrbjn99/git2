76c457e67fa3a66de2c13a0351b684bc4767c407
mrbjn99 <58242504+mrbjn99@users.noreply.github.com>
2019-11-30 07:50:23 +0700

    Add file git2.sln\

---
+++ git2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
